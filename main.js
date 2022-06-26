f1f1c8b33436e91cb7a85be6d3f926b39787ad30
Victor Valeev <viktorval2001@gmail.com>
2022-06-26 11:22:47 +0300

    vars

--- main.js
+++ main.js
@@ -357,7 +357,7 @@ async function update() {
                 // console.log(playerUrl);
             }
 
-            regameText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 100, 'Restart game', { fontSize: '48px', fill: '#FFF' })
+            regameText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 10, 'Restart game', { fontSize: '48px', fill: '#FFF' })
                 .setOrigin(0.5)
                 .setPadding(10)
                 .setStyle({ backgroundColor: '#555' })
